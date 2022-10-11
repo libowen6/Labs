cadafae17aa581d64dd17e0655ded701f1027c8c
714244 <BOWEN.LI-2021@hull.ac.uk>
2022-10-11 19:26:41 +0100

    2022.10.1118.36

--- app/src/main/java/parker/david/labs/CoinMainActivity.java
+++ app/src/main/java/parker/david/labs/CoinMainActivity.java
@@ -2,24 +2,33 @@ package parker.david.labs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.util.Log;
-import java.util.Random;
-import  android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+import android.view.View;
+import android.widget.TextView;
+public class CoinMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_coin_toss);
+        Log.i( "Activity Lifecycle",  "onCreate");
+    }
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        Log.i("Activity Lifecycle","onPause");
     }
     Random random;
     private String getCoinToss(){
-
-        if(random.nextBoolean()){
-            return "Heads!";
+        if(random.nextBoolean()) {
+            return "Heads";
         }
         return "Tails";
     }
@@ -28,7 +37,15 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.i("Activity Lifecycle","onResume");
         TextView coinTossView = (TextView) findViewById(R.id.coinTossView);
+
         String result = getCoinToss();
+
         coinTossView.setText(result);
     }
+    public void openCoinToss(View view){
+        Intent openCoinTossIntent=new Intent(getApplicationContext(),CoinTossACtivity.class);
+        startActivity(openCoinTossIntent);
+    }
+
+
 }
