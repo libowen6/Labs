9d3d350800da1b4a1d6b53dd236d891fc4dd0bd7
714244 <BOWEN.LI-2021@hull.ac.uk>
2022-10-11 10:51:22 +0100

    2022.10.11xiake

--- app/src/main/java/parker/david/labs/MainActivity.java
+++ app/src/main/java/parker/david/labs/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+
+import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i( "Activity Lifecycle",  "onCreate");
+    }
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        Log.i("Activity Lifecycle","onPause");
+    }
+    private String getCoinToss(){
+        if(random.nextBoolean()) {
+            return "Heads";
+        }
+        return "Tails";
     }
 }
